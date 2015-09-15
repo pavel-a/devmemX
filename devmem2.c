e49309ba42a534e5fa31fdaf80011b335c8e7f5b
pavel <pavel@aaa>
2015-09-16 02:25:55 +0300

    Refactoring, fixed #1, validate parameters, added --version

--- devmem2.c
+++ devmem2.c
@@ -32,27 +32,28 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
 #include <fcntl.h>
-#include <ctype.h>
-#include <termios.h>
 #include <sys/types.h>
 #include <sys/mman.h>
+#include <stdint.h>
 
-#define FATAL do { fprintf(stderr, "Error at line %d, file %s (%d) [%s]\n", \
-  __LINE__, __FILE__, errno, strerror(errno)); exit(1); } while(0)
+#define printerr(fmt,...) do { fprintf(stderr, fmt, ## __VA_ARGS__); fflush(stderr); } while(0)
 
-#define MAP_SIZE 4096UL
-#define MAP_MASK (MAP_SIZE - 1)
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int fd;
     void *map_base, *virt_addr;
     unsigned long read_result, writeval;
-    off_t target;
+    uint64_t target;
     int access_type = 'w';
+    int access_size = 0;
+    unsigned int pagesize = (unsigned)getpagesize(); /* or sysconf(_SC_PAGESIZE)  */
+    unsigned int map_size = pagesize;
+    unsigned offset;
+    char *endp = NULL;
+    int readback = 0; // flag to read back after write
 
-    if(argc < 2) {
+    if (argc < 2) {
         fprintf(stderr, "\nUsage:\t%s { address } [ type [ data ] ]\n"
             "\taddress : memory address to act upon\n"
             "\ttype    : access operation type : [b]yte, [h]alfword, [w]ord\n"
@@ -60,66 +61,130 @@ int main(int argc, char **argv) {
             argv[0]);
         exit(1);
     }
-    target = strtoul(argv[1], 0, 0);
 
-    if(argc > 2)
-        access_type = tolower(argv[2][0]);
-
-
-    if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) FATAL;
-    //printf("/dev/mem opened.\n"); 
-    //fflush(stdout);
-
-    /* Map one page */
-    map_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target & ~MAP_MASK);
-    if(map_base == (void *) -1) FATAL;
-    //printf("Memory mapped at address %p.\n", map_base); 
-    //fflush(stdout);
-
-    virt_addr = map_base + (target & MAP_MASK);
-    switch(access_type) {
-        case 'b':
-            read_result = *((unsigned char *) virt_addr);
-            break;
-        case 'h':
-            read_result = *((unsigned short *) virt_addr);
-            break;
-        case 'w':
-            read_result = *((unsigned long *) virt_addr);
-            break;
-        default:
-            fprintf(stderr, "Illegal data type '%c'.\n", access_type);
-            exit(2);
+    if (strcasestr(argv[1], "-v")) {
+        printf("devmem version T/C x.1\n");
+        exit(0);
     }
-    //printf("Value at address 0x%lld (%p): 0x%lu\n", (long long)target, virt_addr, read_result);
-    //fflush(stdout);
 
-    if(argc > 3) {
-        writeval = strtoul(argv[3], 0, 0);
-        switch(access_type) {
+    target = strtoull(argv[1], &endp, 0);
+    if (target == 0 || target >= UINT64_MAX || (endp && 0 != *endp)) {
+        printerr("Invalid memory address: %s\n", argv[1]);
+        exit(2);
+    }
+
+    if ( (sizeof(off_t) < sizeof(int64_t)) && (target > UINT32_MAX) ) {
+        printerr("The address %s is too large. Try to rebuild in 64-bit mode.\n", argv[1]);
+        exit(2);
+    }
+
+    if (argc > 2) {
+        switch(argv[2][0]) {
             case 'b':
-                *((unsigned char *) virt_addr) = writeval;
-                read_result = *((unsigned char *) virt_addr);
-                break;
-            case 'h':
-                *((unsigned short *) virt_addr) = writeval;
-                read_result = *((unsigned short *) virt_addr);
+            case 'B':
+                access_type = 'b';
+                access_size = 1;
                 break;
             case 'w':
-                *((unsigned long *) virt_addr) = writeval;
-                read_result = *((unsigned long *) virt_addr);
+            case 'W':
+                access_type = 'w';
+                access_size = 4;
+                break;
+            case 'h':
+            case 'H':
+                access_type = 'h';
+                access_size = 2;
+                break;
+            default:
+                printerr("Illegal data type: %s\n", argv[2]);
+                exit(2);
+        }
+    }
+
+    offset = (unsigned int)(target & (pagesize-1));
+    if (offset + access_size > pagesize ) {
+        // Access straddles page boundary:  add another page:
+        map_size += pagesize;
+    }
+
+    if (offset & (access_size - 1)) {
+        printerr("WARNING: address not aligned on %d!\n", access_size);
+        //exit(2);
+    }
+
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1) {
+        printerr("Error opening /dev/mem (%d) : %s\n", errno, strerror(errno));
+        exit(1);
+    }
+    //printf("/dev/mem opened.\n");
+    //fflush(stdout);
+
+    map_base = mmap(0, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target & ~((typeof(target))pagesize-1));
+    if (map_base == (void *) -1) {
+        printerr("Error mapping (%d) : %s\n", errno, strerror(errno));
+        exit(1);
+    }
+    //printf("Memory mapped at address %p.\n", map_base);
+    //fflush(stdout);
+
+    virt_addr = map_base + offset;
+
+    if(argc > 3) {
+        errno = 0;
+        writeval = strtoul(argv[3], &endp, 0);
+        if (errno || (endp && 0 != *endp)) {
+            printerr("Invalid data value: %s\n", argv[3]);
+            exit(2);
+        }
+
+        switch(access_type) {
+            case 'b':
+                *((uint8_t *) virt_addr) = writeval;
+                if (readback)
+                    read_result = *((uint8_t *) virt_addr);
+                break;
+            case 'h':
+                *((uint16_t *) virt_addr) = writeval;
+                if (readback)
+                    read_result = *((uint16_t *) virt_addr);
+                break;
+            case 'w':
+                *((uint32_t *) virt_addr) = writeval;
+                if (readback)
+                    read_result = *((uint32_t *) virt_addr);
                 break;
         }
-        //printf("Written 0x%lu; readback 0x%lu\n", writeval, read_result);
+
+        if (readback)
+            printf("Written 0x%lu; readback 0x%lu\n", writeval, read_result);
+        //else
+        //    printf("Written 0x%lu\n", writeval);
         //fflush(stdout);
-	}
+    }
     else
     {
-        printf("%08lX\n", read_result); 
+        switch(access_type) {
+            case 'b':
+                read_result = *((uint8_t *) virt_addr);
+                break;
+            case 'h':
+                read_result = *((uint16_t *) virt_addr);
+                break;
+            case 'w':
+                read_result = *((uint32_t *) virt_addr);
+                break;
+        }
+        //printf("Value at address 0x%lld (%p): 0x%lu\n", (long long)target, virt_addr, read_result);
+        //fflush(stdout);
+        printf("%08lX\n", read_result);
         fflush(stdout);
     }
 
-    if(munmap(map_base, MAP_SIZE) == -1) FATAL;
+    if(munmap(map_base, map_size) == -1) {
+        printerr("ERROR munmap (%d) %s\n", errno, strerror(errno));
+    }
+
     close(fd);
     return 0;
 }
