bb74fc31b032a895946dae23a8169773769a29fb
pavel <pavel@aaa>
2015-09-22 19:55:13 +0300

    Using getopt() to be nice...

--- devmem2.c
+++ devmem2.c
@@ -39,21 +39,16 @@
 
 #define printerr(fmt,...) do { fprintf(stderr, fmt, ## __VA_ARGS__); fflush(stderr); } while(0)
 
-#define VER_STR "devmem version T/C (http://git.io/vZ5iD) rev.0.9x"
+#define VER_STR "devmem version T/C (http://git.io/vZ5iD) rev.0.91x"
 
 // This rebase thing is here to support shell scripts written for other system where the addresses are different.
 // To avoid rewriting old scripts or doing math in sh (brrr)  you can now set env. variables for old and new base address.
 // Old base ($DEVMEMREBASE) will be subtracted and new base ($DEVMEMBASE) will be added.
 // Or even simpler, use only offsets (0-based) and set the base in environment. We'll do the math for you.
-// 
-// TODO define the (real) limits to access with this program. 
-// Knowing the real limits will also let know whether the addresses are 32 or 64-bit.
 
 #define ENV_MBASE   "DEVMEMBASE"
 #define ENV_REBASE  "DEVMEMREBASE"
 #define ENV_PARAMS  "DEVMEMPARAMS"
-//#define ENV_ABSSTART "DEVMEMWIN"
-//#define ENV_ABSEND   "DEVMEMWINEND"
 
 // Global flags
 int f_readback = 0;       // flag to read back after write
@@ -61,14 +56,18 @@ int f_align_check = 1;    // flag to require alignment
 int f_absolute = 0;       // legacy mode
 int f_dbg = 0;
 uint64_t mbase = UINT64_C(-1); // offset to add to the address parameter
-uint64_t mrebase = 0;     // address to rebase from (subtract)
+uint64_t mrebase = 0;     // address to rebase from (subtract this from given address)
 
+// TODO define the (real) limits to access with this program. 
+// Knowing the real limits will also let know whether the addresses are 32 or 64-bit.
+//#define ENV_ABSSTART "DEVMEMWIN"
+//#define ENV_ABSEND   "DEVMEMWINEND"
 //uint64_t mabsstart = ~0U; // phys window start
 //uint64_t mabssize = ~0U;  // phys window size
 
 static void get_env_params(int fPrint);
 
-static void usage(const char *progname)
+static void usage(const char *cmd)
 {
     fprintf(stderr, "\nUsage:\t%s [-switches] address [ type [ data ] ]\n"
         "\taddress : memory address to act upon\n"
@@ -77,11 +76,11 @@ static void usage(const char *progname)
         "Switches:\n"
         "\t-r      : read back after write\n"
         "\t-a      : do not check alignment\n"
-        "\t-A      : \"old\" mode - use absolute addresses\n"
+        "\t-A      : \"old\" mode - using absolute addresses\n"
         "\t--version | -V : print version\n"
-        "\t-d      : debug spew on (use to test the address rebasing)\n"
+        "\t-d      : debug spew on (use it to test the address rebasing)\n"
         "\n",
-        progname);
+        cmd);
 
     get_env_params(1);
 }
@@ -99,53 +98,53 @@ int main(int argc, char **argv)
     unsigned offset;
     char *endp = NULL;
     const char *progname = argv[0];
+    int opt;
 
-    for ( ; argc > 1 && argv[1][0] == '-'; argv++, argc--) {
-        const char * psw = argv[1];
-        if (0 == strcmp(psw, "-r")) {
+    opterr = 0;
+    while ((opt = getopt(argc, argv, "+raAdV")) != -1) {
+        switch(opt) {
+        case 'r':
             f_readback = 1;
-            continue;
-        }
-        
-        if (0 == strcmp(psw, "-a")) {
+            break;
+        case 'a':
             f_align_check = 0;
-            continue;
-        }
-
-        if (0 == strcmp(psw, "-A")) {
+            break;
+        case 'A':    
             f_absolute = 1;
             //? f_align_check = 0;
-            continue;
-        }
-
-        if (0 == strcmp(psw, "-d")) {
+            break;
+        case 'd':
             f_dbg = 1;
-            continue;
-        }
-        
-        if (0 == strncmp(psw, "--vers", 6) || 0 == strcmp(psw, "-V")) {
+            break;
+        case 'V':    
             printf(VER_STR "\n");
             exit(0);
+        default:   
+            if ( (!argv[optind]) || 0 == strcmp(argv[optind], "--help")) {
+                usage(progname);
+                exit(1);
+            }
+            if (0 == strncmp(argv[optind], "--vers", 6)) {
+                printf(VER_STR "\n");
+                exit(0);
+            }
+            printerr("Unknown long option: %s\n", argv[optind]);
+            exit(2);
         }
-        
-        if (0 == strcmp(psw, "--help") || 0 == strcmp(psw, "-?")) {
-            argc = 0;
-            break;
-        }
-        
-        printerr("Unknown option: %s\n", psw);
-        exit(2);
     }
 
-    if (argc < 2) {
+    if (optind >= argc) {
         usage(progname);
         exit(1);
     }
     
+    argc -= optind - 1;
+    argv += optind - 1;
+    
     get_env_params(0);
     
     if (argc > 2) {
-        if (isalpha(argv[1][0])) {
+        if (!isdigit(argv[1][0])) {
             // Allow access_type be 1st arg, then swap 1st and 2nd
             char *t = argv[2];
             argv[2] = argv[1];
@@ -218,6 +217,10 @@ int main(int argc, char **argv)
         map_size += pagesize;
     }
 
+    if (f_dbg) {
+        printerr("Address: %#" PRIX64 " op.size=%d\n", target, access_size);
+    }
+
     if (f_align_check && offset & (access_size - 1)) {
         printerr("ERROR: address not aligned on %d!\n", access_size);
         exit(2);
@@ -268,8 +271,10 @@ int main(int argc, char **argv)
                 break;
         }
 
-        //    printf("Written 0x%lu\n", writeval);
-        //fflush(stdout);
+        if (f_dbg) {
+            printerr("Address: %#" PRIX64 " Written: %#lX\n", target, writeval);
+            fflush(stdout);
+        }
     }
     
     if (argc <= 3 || f_readback) {
@@ -287,7 +292,7 @@ int main(int argc, char **argv)
 
         //printf("Value at address 0x%lld (%p): 0x%lu\n", (long long)target, virt_addr, read_result);
         //fflush(stdout);
-        if (f_readback)
+        if (f_readback && argc > 3)
             printf("Written 0x%lx; readback 0x%lx\n", writeval, read_result);
         else
             printf("%08lX\n", read_result);
@@ -322,10 +327,10 @@ static void get_env_params(int a_print)
         if(fPrint)
             printf("%s = \"%s\" (all ignored!)\n", ENV_PARAMS, p);
         
-        // TODO parse params
+        // TODO parse params... can put here debug, alignment...
     }
-    else if (fPrint)
-        printf("%s not set\n", ENV_PARAMS);
+    //else if (fPrint)
+    //    printf("%s not set\n", ENV_PARAMS);
         
     p = getenv(ENV_MBASE);
     if (p) {
